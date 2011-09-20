028b958cb787a0977d56f0e2fead80916d711495
zhangjieinnjucs <ndzj981479673@gmail.com>
2011-09-20 20:11:11 +0800

    make store

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ActiveRecord::Base
+	default_scope :order => 'title'
 validates :title, :description, :image_url, :presence => true
 validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
 validates :title, :uniqueness => true
@@ -7,5 +8,5 @@ validates :image_url, :format => {
 :with => %r{\.(gif|jpg|png)$}i,
 :message => 'must be a URL for GIF, JPG or PNG image.'
 }
-
+validates_length_of :title, :minimum => 10, :message => "at least 10 characters" 
 end
